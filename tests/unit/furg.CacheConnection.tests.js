a2542c71cc35cf2fc209b0d3c55c9125ac40514f
zaphod1984 <zaphod84@gmx.de>
2014-09-20 18:30:46 +0200

    WIP furg

---
+++ tests/unit/furg.CacheConnection.tests.js
@@ -0,0 +1,76 @@
+var expect = require('chai').use(require('sinon-chai')).expect;
+var sinon = require('sinon');
+
+var Cache = require('../../lib/furg/Cache');
+var CacheConnection = require('../../lib/furg/CacheConnection');
+var Pool = require('../../lib/furg/Pool');
+
+describe('furg', function () {
+    describe('cacheConnection', function () {
+        describe('faulty assignments', function () {
+            var a = new CacheConnection();
+
+            it('should throw on invalid dependency', function () {
+                expect(function () {
+                    a.depends([1, 2, 3]);
+                }).to.throw('not a resource');
+            });
+
+            it('should throw on invalid dependency', function () {
+                expect(function () {
+                    a.depends(true);
+                }).to.throw('not a resource');
+            });
+
+            it('should throw on invalid add', function () {
+                expect(function () {
+                    a.adds(true);
+                }).to.throw('not a resource');
+            });
+
+            it('should throw on invalid change', function () {
+                expect(function () {
+                    a.changes(true);
+                }).to.throw('not a resource');
+            });
+        });
+
+        describe('assignments', function () {
+            var a;
+            before(function () {
+                a = new CacheConnection();
+                a.depends({resource: 1}, {resource: 2}, {resource: 3});
+                a.adds({resource: 2});
+                a.changes({resource: 3});
+            });
+
+            it('should add multiple dependencies', function () {
+                expect(a.getDependencies()).to.deep.equal([1, 2, 3]);
+            });
+
+            it('should add', function () {
+                expect(a.getAdd()).to.equal(2);
+            });
+
+            it('should change', function () {
+                expect(a.getChange()).to.deep.equal(3);
+            });
+
+            it('should signal that it is invalidating', function () {
+                expect(a.isInvalidating()).to.be.true;
+            });
+        });
+
+        describe('parallel assignment', function () {
+            var a;
+            before(function () {
+                a = new CacheConnection();
+                a.depends([{resource: 1}, {resource: 2}], {resource: 'ishouldnotbeeseen'});
+            });
+
+            it('should only add the parralel assignment', function () {
+                expect(a.getDependencies()).to.deep.equal([[1, 2]]);
+            });
+        });
+    });
+});
---
+++ tests/unit/furg.Pool.tests.js
@@ -0,0 +1,39 @@
+var expect = require('chai').use(require('sinon-chai')).expect;
+var sinon = require('sinon');
+
+var Resource = require('../../lib/furg/Resource');
+var ResourceQuantifier = require('../../lib/furg/ResourceQuantifier');
+var Pool = require('../../lib/furg/Pool');
+
+describe('furg', function () {
+    describe('Pool', function() {
+        var pool, a;
+
+        before(function() {
+            pool = new Pool();
+
+            a = new Resource('A');
+            pool.register(a);
+        });
+
+        it('should return a quantified Resource /wo quantifier', function() {
+            var expected = new ResourceQuantifier(a);
+
+            expect(pool.A.resource).to.be.deep.equal(expected);
+        });
+
+        it('should return a quantified Resource /w all quantifier', function() {
+            var expected = new ResourceQuantifier(a);
+            expected.setQuantifier('all');
+
+            expect(pool.A.all.resource).to.be.deep.equal(expected);
+        });
+
+        it('should return a quantified Resource /w foo quantifier', function() {
+            var expected = new ResourceQuantifier(a);
+            expected.setQuantifier('foo');
+
+            expect(pool.A('foo').resource).to.be.deep.equal(expected);
+        });
+    });
+});
